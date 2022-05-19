99c5aeaca0f3d62e6c0203f230f442ed6e11684e
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-05-19 19:59:17 -0300

    Component NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+
+
+export const NavBar = () => {
+
+    return <header>
+        <h1>Nombre Pagina</h1>
+        <hr />
+        <nav>
+            <a href="#">Inicio</a>
+            
+            <a href="#">Shop</a>
+            
+            <a href="#">Informacion</a>
+        </nav>
+    </header>
+}
+
+export default NavBar 
