d5b1c0b7b308fe47f9c5b131b212517641d8a9a1
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-05-24 16:00:08 -0300

    Add files via upload

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import CartWidget from '../CartWidget/CartWidget';
+
+export const NavBar = () => {
+
+    return ( 
+        <header className="header">
+            <div className="header__container">
+            <h1>Nombre Pagina</h1>
+        
+            <nav className="header__navbar">
+                <a href="#" className="header__navlink">Inicio</a>
+            
+                <a href="#" className="header__navlink">Shop</a>
+            
+                <a href="#" className="header__navlink">Informacion</a>
+                <CartWidget/>
+            </nav>
+        </div>
+        </header>
+    )
+}
+
+export default NavBar 
